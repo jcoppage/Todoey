deeb35d544ca14b8aa0f5c27b0d9f982e3d65815
user <user@users-mbp>
2020-08-28 00:32:09 -0700

    Data Persisted using user defaults method

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -13,10 +13,17 @@ class TodoListViewController: UITableViewController {
     //let itemArray = ["Find Mike", "Buy Eggs", "Destroy Demogorgon"]
     // making it a VAR istead on a constant, (Making it mutable) so we can add items to it
     var itemArray = ["Find Mike", "Buy Eggs", "Destroy Demogorgon"]
+    
+    let defaults = UserDefaults.standard
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //Use IF statement because the list may not exist and we dont want it to crash
+        if let items = defaults.array(forKey: "TodoListArray") as? [String]{ // Getting the stored data locted with key: "ToDoListArray"
+            itemArray = items
+        }
     }
     
     //MARK - Tableview Datasource Methods
@@ -75,6 +82,8 @@ class TodoListViewController: UITableViewController {
             
             self.itemArray.append(textField.text!) // need to force unwrap it (!) because it may be NIL
             // you can also put: .append(textField.text ?? "New Item") if it is NIL then it'll replace the string with "New Item"
+            
+            self.defaults.set(self.itemArray, forKey: "TodoListArray") //Stroing the Array data in out app's .p list file
             
             self.tableView.reloadData()
             
